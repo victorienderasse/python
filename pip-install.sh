c71ce5b493cfff2269e3e906d6324cec83cb876a
victorienderasse <victorien.derasse@hotmail.com>
2017-04-12 14:34:07 +0200

    pip-install.sh done

--- pip-install.sh
+++ pip-install.sh
@@ -3,5 +3,5 @@
 sudo apt-get install python-pip -y
 sudo pip install socketIO_client
 sudo pip install picamera
-sudo apt-get install python-opencv
-sudo apt-get install python-zbar
+sudo apt-get install python-opencv -y
+sudo apt-get install python-zbar -y
